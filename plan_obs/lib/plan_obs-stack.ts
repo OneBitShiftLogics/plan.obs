6603c7a6a44a6b4a415bf68051cc7d031cd7278d
Onebitshift-1-logics <onebitshift@gmail.com>
2025-03-04 22:02:27 +0530

    API Gateway for Plans management

--- plan_obs/lib/plan_obs-stack.ts
+++ plan_obs/lib/plan_obs-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class PlanObsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB Table
+    const plansTable = new dynamodb.Table(this, 'PlansTable', {
+      tableName: 'Plans',
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Change to RETAIN for production
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PlanObsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda Function
+    const plansLambda = new lambda.Function(this, 'PlansLambda', {
+      functionName: 'PlansLambda',
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'app.lambda_handler', // Python entry point
+      code: lambda.Code.fromAsset('lambda'), // Assumes Python code is in 'lambda/' folder
+      environment: {
+        PLANS_TABLE_NAME: plansTable.tableName,
+      },
+    });
+
+    // Grant Lambda access to DynamoDB
+    plansTable.grantReadWriteData(plansLambda);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'PlansApi', {
+      restApiName: 'Plans API',
+      deployOptions: { stageName: 'test-production' },
+    });
+
+    // CRUD Routes
+    const plansResource = api.root.addResource('plans');
+
+    plansResource.addMethod('POST', new apigateway.LambdaIntegration(plansLambda)); // Create
+    plansResource.addMethod('GET', new apigateway.LambdaIntegration(plansLambda)); // Read all
+
+    const singlePlanResource = plansResource.addResource('{id}');
+    singlePlanResource.addMethod('GET', new apigateway.LambdaIntegration(plansLambda)); // Read one
+    singlePlanResource.addMethod('PUT', new apigateway.LambdaIntegration(plansLambda)); // Update
+    singlePlanResource.addMethod('DELETE', new apigateway.LambdaIntegration(plansLambda)); // Delete
+
+    // Output API URL
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+    });
   }
 }
